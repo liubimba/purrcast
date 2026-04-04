5b92b5074b13727914411e752386ab51c9eadcd5
bimba <bimba@flashphoner.com>
2026-04-04 08:00:41 +0700

    minor changes #MUL-23

--- electron-app/forge.config.ts
+++ electron-app/forge.config.ts
@@ -6,31 +6,24 @@ import {MakerRpm} from '@electron-forge/maker-rpm';
 import {AutoUnpackNativesPlugin} from '@electron-forge/plugin-auto-unpack-natives';
 import {FusesPlugin} from '@electron-forge/plugin-fuses';
 import {FuseV1Options, FuseVersion} from '@electron/fuses';
-
+import {WebpackPlugin as ForgeWebpackPlugin} from '@electron-forge/plugin-webpack';
 import {mainConfig} from './webpack.main.config';
 import {rendererConfig} from './webpack.renderer.config';
-import * as path from "node:path";
-import {spawnSync} from "child_process";
-import WebpackPlugin from "@electron-forge/plugin-webpack";
+import {spawnSync} from 'node:child_process';
+import * as path from 'node:path';
 
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
-        extraResource: [
-            "./resources/bin/",
-            "./resources/config",
-            "./resources/static"
-        ]
+        extraResource: ["./resources/bin/", "./resources/config", "./resources/static"]
     },
     hooks: {
         generateAssets: async () => {
             const script = path.join(__dirname, "scripts", "copy-artifacts.js");
             console.log("Running copy-artifacts.js...");
             const result = spawnSync("node", [script], {stdio: "inherit"});
-            if (result.status !== 0) {
-                throw new Error(`Failed to copy artifacts. Error: ${result.error}`);
-            }
-        }
+            if (result.status !== 0) throw new Error(`Failed to copy artifacts. Error: ${result.error}`);
+        },
     },
     rebuildConfig: {},
     makers: [
@@ -41,7 +34,7 @@ const config: ForgeConfig = {
     ],
     plugins: [
         new AutoUnpackNativesPlugin({}),
-        new WebpackPlugin({
+        new ForgeWebpackPlugin({
             mainConfig,
             renderer: {
                 config: rendererConfig,
@@ -50,15 +43,11 @@ const config: ForgeConfig = {
                         html: './src/index.html',
                         js: './src/renderer.ts',
                         name: 'main_window',
-                        preload: {
-                            js: './src/preload.ts',
-                        },
+                        preload: {js: './src/preload.ts'},
                     },
                 ],
             },
         }),
-        // Fuses are used to enable/disable various Electron functionality
-        // at package time, before code signing the application
         new FusesPlugin({
             version: FuseVersion.V1,
             [FuseV1Options.RunAsNode]: false,
